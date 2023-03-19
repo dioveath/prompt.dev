d3280003af0e58efaf5d5ba69d92a8d2705047d5
Saroj Rai <raisaroj360@gmail.com>
2023-03-19 23:25:09 +0000

    add: prisma, graphql, pothos

---
+++ graphql/types/User.ts
@@ -0,0 +1,26 @@
+import { PrismaFieldResolver } from "@pothos/plugin-prisma";
+import PrismaTypes from "@pothos/plugin-prisma/generated";
+import { builder } from "../builder";
+
+builder.prismaObject('User', {
+    fields: (t) => ({
+        id: t.exposeID('id'),
+        email: t.exposeString('email'),
+        name: t.exposeString('name', { nullable: true }),
+        avatar: t.exposeString('avatar', { nullable: true }),
+        jobTitle: t.exposeString('jobTitle', { nullable: true }),
+        createdAt: t.expose('createdAt', { type: 'String' }),
+        updatedAt: t.expose('updatedAt', { type: 'String' }),
+    })
+});
+
+
+builder.queryField('users', (t) => 
+    t.prismaField({
+        type: ['User'],
+        resolve: async (query, _parent, _args, _ctx, _info) => {
+            const users = await prisma.user.findMany({ ...query });
+            return users;
+        }
+    })
+);
---
+++ prisma/seed.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      email: "test@gmail.com",
+      password: "test123",
+      name: "Test",
+      avatar: "https://avatars.githubusercontent.com/u/6702424?v=4",
+      jobTitle: "Software Engineer",
+    },
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
